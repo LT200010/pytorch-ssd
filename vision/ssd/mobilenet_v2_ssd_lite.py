dbe3c8db5c2d4b684bda1a1941a830f147098093
LT200010 <90982174+LT200010@users.noreply.github.com>
2023-05-02 00:24:54 +0800

    Update mobilenet_v2_ssd_lite.py

--- vision/ssd/mobilenet_v2_ssd_lite.py
+++ vision/ssd/mobilenet_v2_ssd_lite.py
@@ -8,16 +8,16 @@ from .predictor import Predictor
 from .config import mobilenetv1_ssd_config as config
 
 
-def SeperableConv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, onnx_compatible=False):
+def SeperableConv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, onnx_compatible=False): #深度可分离卷积
     """Replace Conv2d with a depthwise Conv2d and Pointwise Conv2d.
     """
     ReLU = nn.ReLU if onnx_compatible else nn.ReLU6
     return Sequential(
         Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size,
-               groups=in_channels, stride=stride, padding=padding),
-        BatchNorm2d(in_channels),
-        ReLU(),
-        Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1),
+               groups=in_channels, stride=stride, padding=padding),  #深度卷积（depthwise convolution）：使用Conv2d进行卷积操作，设置输入通道数为in_channels，输出通道数也为in_channels，卷积核大小为kernel_size，组数（groups）设置为输入通道数，步长为stride，填充大小为padding。
+        BatchNorm2d(in_channels), #归一化（batch normalization）：使用BatchNorm2d对深度卷积的输出进行归一化操作，输入通道数为in_channels。
+        ReLU(), #激活函数
+        Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1), #逐点卷积（pointwise convolution）：使用Conv2d进行卷积操作，设置输入通道数为in_channels，输出通道数为out_channels，卷积核大小为1。
     )
 
 
@@ -28,7 +28,7 @@ def create_mobilenetv2_ssd_lite(num_classes, width_mult=1.0, use_batch_norm=True
     source_layer_indexes = [
         GraphPath(14, 'conv', 3),
         19,
-    ]
+    ] #GraphPath(14, 'conv', 3)表示从基础网络的第14个模块的conv层的第3个输出位置添加额外的层。19表示在基础网络的第19个模块之后添加额外的层。
     extras = ModuleList([
         InvertedResidual(1280, 512, stride=2, expand_ratio=0.2),
         InvertedResidual(512, 256, stride=2, expand_ratio=0.25),
